1e9a66553383efff1b11f295c2c834ea6bfacd94
Valmy-Roi Kenfack <roykenvalmy@gmail.com>
2017-05-13 20:26:46 +0100

    update

--- src/pages/tracking/tracking.ts
+++ src/pages/tracking/tracking.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams,Platform } from 'ionic-angular';
 import { LocationTracker } from '../../providers/location-tracker';
+import { Geolocation } from '@ionic-native/geolocation';
+import { GoogleMaps, GoogleMap, GoogleMapsEvent, LatLng, CameraPosition, MarkerOptions, Marker } from '@ionic-native/google-maps';
 
 /*
   Generated class for the Tracking page.
@@ -14,10 +16,17 @@ import { LocationTracker } from '../../providers/location-tracker';
 })
 export class TrackingPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public locationTracker: LocationTracker) {}
+	map: GoogleMap;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,public locationTracker: LocationTracker,private geolocation: Geolocation,public platform: Platform) {
+  	
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TrackingPage');
+    this.platform.ready().then(() => {
+    	this.loadMap();
+  	});
   }
 
    start(){
@@ -27,5 +36,40 @@ export class TrackingPage {
   stop(){
     this.locationTracker.stopTracking();
   }
+
+  loadMap() {
+  this.geolocation.getCurrentPosition({ maximumAge: 3000, timeout: 5000, enableHighAccuracy: true }).then((resp) => {
+    console.log(resp.coords.latitude+", "+resp.coords.longitude);
+    let location = new LatLng(resp.coords.latitude, resp.coords.longitude);
+
+    this.map = new GoogleMap('map', {
+      'backgroundColor': 'white',
+      'controls': {
+        'compass': true,
+        'myLocationButton': true,
+        'indoorPicker': true,
+        'zoom': true
+      },
+      'gestures': {
+        'scroll': true,
+        'tilt': true,
+        'rotate': true,
+        'zoom': true
+      },
+      'camera': {
+        'latLng': location,
+        'tilt': 30,
+        'zoom': 15,
+        'bearing': 50
+      }
+    });
+
+    this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+      console.log('Map is ready!');
+    });
+  }).catch((error) => {
+    console.log('Error getting location', error);
+  });
+}
 
 }
