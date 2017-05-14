073724b609374369c15796cf54234cc7c3c83f69
Valmy-Roi Kenfack <roykenvalmy@gmail.com>
2017-05-14 17:14:17 +0100

    Error in app.module corrected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
-import { NgModule} from '@angular/core';
-import { IonicApp, IonicModule} from 'ionic-angular';
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { LoginPage } from '../pages/login/login';
-import { AuthService } from '../providers/auth-service';
-import { RegisterPage } from '../pages/register/register';
+import {NgModule} from '@angular/core';
+import {IonicApp, IonicModule} from 'ionic-angular';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {LoginPage} from '../pages/login/login';
+import {AuthService} from '../providers/auth-service';
+import {RegisterPage} from '../pages/register/register';
 import {ConfirmationPage} from '../pages/confirmation/confirmation';
-import { BookingPage } from '../pages/booking/booking';
-import { ChatsPage } from '../pages/chats/chats';
+import {BookingPage} from '../pages/booking/booking';
+import {ChatsPage} from '../pages/chats/chats';
 //import { MapsPage } from '../pages/maps/maps';
-import { MeteoPage } from '../pages/meteo/meteo';
-import { HttpModule, JsonpModule } from '@angular/http';
+import {MeteoPage} from '../pages/meteo/meteo';
+import {HttpModule, JsonpModule} from '@angular/http';
 //import { StationListPage } from '../pages/station-list/station-list';
-import { StationMapPage } from '../pages/station-map/station-map';
-import { Locations } from '../providers/locations';
-import { GoogleMaps } from '../providers/google-maps';
-import { Connectivity } from '../providers/connectivity';
-import { LocationTracker } from '../providers/location-tracker';
-import { BackgroundGeolocation } from '@ionic-native/background-geolocation';
-import { Geolocation } from '@ionic-native/geolocation';
-import { TrackingPage } from '../pages/tracking/tracking';
+import {StationMapPage} from '../pages/station-map/station-map';
+import {Locations} from '../providers/locations';
+import {GoogleMaps} from '../providers/google-maps';
+import {Connectivity} from '../providers/connectivity';
+import {LocationTracker} from '../providers/location-tracker';
+import {BackgroundGeolocation} from '@ionic-native/background-geolocation';
+import {Geolocation} from '@ionic-native/geolocation';
+import {TrackingPage} from '../pages/tracking/tracking';
 import {Database} from "../providers/database";
 
 // les modules de la meteo
@@ -30,17 +30,10 @@ import {ChartComponent} from '../components/ChartComponent';
 @NgModule({
 
   imports: [
-    HttpModule,
-    JsonpModule,
-    IonicModule.forRoot(MyApp, {
-        //backButtonText: 'Go Back',
-        //iconMode: 'ios',
-        //modalEnter: 'modal-slide-in',
-        //modalLeave: 'modal-slide-out',
-        //tabsPlacement: 'top',
-        //pageTransition: 'ios'
-      }, {}
-    )
+    HttpModule, JsonpModule, IonicModule.forRoot(MyApp, {
+      // backButtonText: 'Go Back', iconMode: 'ios', modalEnter: 'modal-slide-in',
+      // modalLeave: 'modal-slide-out', tabsPlacement: 'top', pageTransition: 'ios'
+    }, {})
   ],
   declarations: [
     MyApp,
@@ -48,11 +41,9 @@ import {ChartComponent} from '../components/ChartComponent';
     LoginPage,
     RegisterPage,
     ConfirmationPage,
-   StationListPage,
     StationMapPage,
     BookingPage,
     ChatsPage,
-    MapsPage,
     MeteoPage,
     ChartComponent,
     TrackingPage
@@ -65,18 +56,22 @@ import {ChartComponent} from '../components/ChartComponent';
     LoginPage,
     RegisterPage,
     ConfirmationPage,
-    StationListPage,
     StationMapPage,
     BookingPage,
     ChatsPage,
-    MapsPage,
     MeteoPage,
-    TrackingPage,
-
-
+    TrackingPage
   ],
-  providers: [AuthService, Locations, GoogleMaps, Connectivity, LocationTracker,BackgroundGeolocation,
-    Geolocation, WeatherService,Database],
-
+  providers: [
+    AuthService,
+    Locations,
+    GoogleMaps,
+    Connectivity,
+    LocationTracker,
+    BackgroundGeolocation,
+    Geolocation,
+    WeatherService,
+    Database
+  ]
 })
 export class AppModule {}
